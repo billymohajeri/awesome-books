a9d8a0e6f5ad01cdf20a9ff1b3c25780e2502ddb
Mohammad (Billy) Mohajeri <mohajeri322@gmail.com>
2022-11-28 16:59:57 +0200

    Finilized the solution

--- script.js
+++ script.js
@@ -5,18 +5,40 @@ const add = document.getElementById('add');
 
 const booksFromLocalStorage = JSON.parse(localStorage.getItem('books'));
 
+function showBooks() {
+  if (booksFromLocalStorage) {
+    for (let index = 0; index < booksFromLocalStorage.length; index += 1) {
+      const savedBooksSection = document.getElementById('saved-books');
+      const titleP = document.createElement('p');
+      titleP.innerText = booksFromLocalStorage[index].title;
+      titleP.innerText += `\n${booksFromLocalStorage[index].author}`;
+      const btnRemove = document.createElement('button');
+      btnRemove.innerText = 'Remove';
+      btnRemove.className = 'remove-button';
+      btnRemove.addEventListener('click', () => {
+        booksFromLocalStorage.splice(index, 1);
+        localStorage.setItem('books', JSON.stringify(booksFromLocalStorage));
+        document.location.reload();
+      });
+      const hr = document.createElement('hr');
+      savedBooksSection.append(titleP, btnRemove, hr);
+    }
+  }
+}
+
+showBooks();
 
 add.addEventListener('click', () => {
-
-const newBook = {
-    // id: booksFromLocalStorage ? booksFromLocalStorage.lenght + 1 : 1,
+  const newBook = {
     title: titles.value,
-    author: authors.value
-}
-   if(booksFromLocalStorage) {
-    localStorage.setItem('books', JSON.stringify([...booksFromLocalStorage, newBook]));
-    
-   } else {
+    author: authors.value,
+  };
+  if (booksFromLocalStorage) {
+    localStorage.setItem(
+      'books',
+      JSON.stringify([...booksFromLocalStorage, newBook]),
+    );
+  } else {
     localStorage.setItem('books', JSON.stringify([newBook]));
-   }
+  }
 });
