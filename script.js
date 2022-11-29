accae7f60b3395fafb673d603e26e3c3a235c9d0
Mohammad (Billy) Mohajeri <mohajeri322@gmail.com>
2022-11-29 15:27:38 +0200

    Add styling

--- script.js
+++ script.js
@@ -7,57 +7,57 @@ const booksFromLocalStorage = JSON.parse(localStorage.getItem('books'));
 
 const savedBooksSection = document.getElementById('saved-books');
 
-// Class of book 
+// Class of book
 class Books {
-    // constructor which execute directely when new create keyword 
-    constructor (title, author)
-    {
-        // id wich we need for remove btn as well 
-        this.book = {id: this.allBooks.length + 1, title, author}   
-    }
+  // constructor which execute directely when new create keyword
+  constructor(title, author) {
+    // id wich we need for remove btn as well
+    this.book = { id: this.allBooks.length + 1, title, author };
+  }
 
-    // Cheak local localStorage 
-    allBooks = booksFromLocalStorage ? booksFromLocalStorage : [];
+  // Cheak local localStorage
+  allBooks = booksFromLocalStorage || [];
 
-    add() {
-        this.allBooks.push(this.book)
-        localStorage.setItem('books', JSON.stringify(this.allBooks));
-    }
+  add() {
+    this.allBooks.push(this.book);
+    localStorage.setItem('books', JSON.stringify(this.allBooks));
+  }
 
-    remove(bookId) {
-        this.allBooks = this.allBooks.filter((book) => book.id !== bookId);
-        localStorage.setItem('books', JSON.stringify(this.allBooks));
-        savedBooksSection.innerHTML = '';
-    }
+  remove(bookId) {
+    this.allBooks = this.allBooks.filter((book) => book.id !== bookId);
+    localStorage.setItem('books', JSON.stringify(this.allBooks));
+    savedBooksSection.innerHTML = '';
+  }
 }
 
 const book = new Books(titles.value, authors.value);
 
 function showBooks() {
-  if (booksFromLocalStorage) {
+  if (book.allBooks.length) {
     for (let index = 0; index < book.allBooks.length; index += 1) {
+      const bookArticle = document.createElement('article');
       const titleP = document.createElement('p');
-      titleP.innerText = book.allBooks[index].title;
-      titleP.innerText += `\n${book.allBooks[index].author}`;
+      titleP.innerText = `"${book.allBooks[index].title}`;
+      titleP.innerText += `" by ${book.allBooks[index].author}`;
       const btnRemove = document.createElement('button');
       btnRemove.innerText = 'Remove';
       btnRemove.className = 'remove-button';
       btnRemove.addEventListener('click', () => {
         book.remove(book.allBooks[index].id);
-
-        showBooks()
+        showBooks();
       });
-      const hr = document.createElement('hr');
-      savedBooksSection.append(titleP, btnRemove, hr);
+      savedBooksSection.append(bookArticle);
+      bookArticle.append(titleP, btnRemove);
     }
+  } else {
+    savedBooksSection.style.display = 'none';
   }
 }
 
 showBooks();
 
-
 add.addEventListener('click', () => {
-    book.book.title = titles.value;
-    book.book.author = authors.value;
-    book.add();
+  book.book.title = titles.value;
+  book.book.author = authors.value;
+  book.add();
 });
